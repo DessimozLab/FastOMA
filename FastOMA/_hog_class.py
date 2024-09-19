64955337ec204868135f76dc9f193d1d8aa82572
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2024-09-19 09:52:06 +0200

    make sure root node of species tree has the correct nr of leaves (pruned on mrca, not root)

--- FastOMA/_hog_class.py
+++ FastOMA/_hog_class.py
@@ -91,7 +91,6 @@ class HOG:
             self._msa = MultipleSeqAlignment([only_protein])
             self._subhogs = []
             self._dubious_members = set()
-            self._num_species_tax_speciestree = 1  # at the leaf level, there is only one species in the species tree
             # <<class 'Bio.Align.MultipleSeqAlignment'> instance (1 records of length 314) at 7f0e86c713d0>
 
         elif msa and all(isinstance(x, HOG) for x in input_instantiate):
@@ -103,7 +102,6 @@ class HOG:
                 hog_members |= sub_hog.get_members()
             self._members = hog_members  # set.union(*tup)    # this also include dubious_members
             self._subhogs = list(input_instantiate)  # full members of subhog, children
-            self._num_species_tax_speciestree = taxnomic_range.size
             self._representatives = [r for r in representatives]
             representatives_id = set(r.get_id() for r in representatives)
             assert len(representatives_id) == len(self._representatives), "Non-unique ids among representatives"
@@ -338,7 +336,7 @@ class HOG:
             #hog_elemnt = ET.Element('orthologGroup', attrib={"id": str(self._hogid)})
             hog_elemnt = ET.Element('orthologGroup', attrib={"id": str(self._hogid)}, )
             num_species_tax_hog = len(set([i.split("||")[1] for i in self._members]))  #  'tr|H2MU14|H2MU14_ORYLA||ORYLA||1056022282'
-            completeness_score = round(num_species_tax_hog/self._num_species_tax_speciestree, 4)
+            completeness_score = round(num_species_tax_hog/self._tax_now.size, 4)
             property_element = ET.SubElement(hog_elemnt, "score", attrib={"id": "CompletenessScore", "value": str(completeness_score)})
             property_element = ET.SubElement(hog_elemnt, "property", attrib={"name": "TaxRange", "value": str(self._tax_now.name)})
 
